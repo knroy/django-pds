0d7d17b016ceb6f134c60af75a8dc11ac7425539
Komol Nath Roy <rax.komol@gmail.com>
2020-05-01 00:16:01 +0600

    init py updated with configuration

---
+++ django_pds/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'Django Platform Data Service'
+__version__ = '0.0.1'
+__author__ = 'Komol Nath Roy'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Komol Nath Roy'
+
+VERSION = __version__
+
+default_app_config = 'django_pds.apps.DjangoPdsConfig'
