18d4a26e2f6d002c529f8f901d2c1a3516f801eb
Komol Nath Roy <rax.komol@gmail.com>
2020-04-30 23:55:19 +0600

    core base api view renamed

--- django_pds/core/rest/views.py
+++ django_pds/core/rest/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView as AV
 from .exceptions import method_not_allowed
 
 
-class APIView(AV):
+class BaseAPIView(AV):
 
     def http_method_not_allowed(self, request, *args, **kwargs):
         return method_not_allowed()
