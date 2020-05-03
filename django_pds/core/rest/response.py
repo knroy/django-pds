beb59f0e298e51e7222af086ffa71978b511b9ce
Komol Nath Roy <rax.komol@gmail.com>
2020-05-04 00:28:15 +0600

    added standard reponse methods

---
+++ django_pds/core/rest/response.py
@@ -0,0 +1,24 @@
+def error_response(message=None, status=None):
+    return {
+        "error_status": status or 400,
+        "error": "bad_request",
+        "error_description": message or "The request is not valid."
+    }
+
+
+def success_response(data=None, message=None, status=None):
+    return {
+        "success_status": status or 200,
+        "error": None,
+        "success_description": message or "request valid",
+        "responsible": data or None
+    }
+
+
+def error_response_read_only_fields(fields, message=None, status=None):
+    return {
+        "error_status": status or 400,
+        "error": "bad_request",
+        "error_description": message or "The request is not valid.",
+        "responsible": fields
+    }
