74cd4a7ef54547646b2a82ce1f7950ee0c310595
Komol Nath Roy <rax.komol@gmail.com>
2020-05-04 01:21:47 +0600

    generic init updated

--- django_pds/core/pds/generic/__init__.py
+++ django_pds/core/pds/generic/__init__.py
@@ -1,7 +1,13 @@
-from .read import get_by_sql_filter_api_view_helper
+from .delete import data_delete_api_view_helper
+from .read import data_read_api_view_helper
+from .update import data_update_api_view_helper
+from .upsert import data_upsert_api_view_helper
 from .write import data_insert_api_view_helper
 
 __all__ = [
+    'data_read_api_view_helper',
     'data_insert_api_view_helper',
-    'get_by_sql_filter_api_view_helper'
+    'data_update_api_view_helper',
+    'data_upsert_api_view_helper',
+    'data_delete_api_view_helper'
 ]
