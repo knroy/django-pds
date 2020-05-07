bda47662a0fc4e2aa4ef29c734a141aaacaa5ede
Komol Nath Roy <rax.komol@gmail.com>
2020-05-07 21:48:36 +0600

    updated, alias removed

--- django_pds/core/rest/views.py
+++ django_pds/core/rest/views.py
@@ -1,9 +1,9 @@
-from rest_framework.views import APIView as AV
+from rest_framework.views import APIView
 
 from .exceptions import method_not_allowed
 
 
-class BaseAPIView(AV):
+class BaseAPIView(APIView):
 
     def http_method_not_allowed(self, request, *args, **kwargs):
         return method_not_allowed()
