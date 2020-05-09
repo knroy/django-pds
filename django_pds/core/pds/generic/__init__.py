3ce97d4b9e2b645c3a87b7ca02d2e12fb6aa04d9
Komol Nath Roy <rax.komol@gmail.com>
2020-05-09 10:16:33 +0600

    updated

--- django_pds/core/pds/generic/__init__.py
+++ django_pds/core/pds/generic/__init__.py
@@ -2,12 +2,13 @@ from .delete import data_delete_api_view_helper
 from .read import data_read_api_view_helper
 from .update import data_update_api_view_helper
 from .upsert import data_upsert_api_view_helper
-from .write import data_insert_api_view_helper
+from .write import data_insert_api_view_helper, simple_data_insert_api_view_helper
 
 __all__ = [
     'data_read_api_view_helper',
     'data_insert_api_view_helper',
     'data_update_api_view_helper',
     'data_upsert_api_view_helper',
-    'data_delete_api_view_helper'
+    'data_delete_api_view_helper',
+    'simple_data_insert_api_view_helper'
 ]
