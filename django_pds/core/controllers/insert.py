94acd13433e445e3e92ba4cf16aac9d02444eb49
Komol Nath Roy <rax.komol@gmail.com>
2020-05-09 10:15:00 +0600

    single and multiple document insert updated

--- django_pds/core/controllers/insert.py
+++ django_pds/core/controllers/insert.py
@@ -1,7 +1,7 @@
 import json
 
+from django_pds.conf import settings
 from django_pds.core.controllers.base import BaseController
-from django_pds.core.settings import SECURITY_IDS_ATTRIBUTES, SECURITY_ROLES_ATTRIBUTES
 
 OWNER = 'Owner'
 
@@ -30,37 +30,46 @@ class GenericInsertCommandController(BaseController):
         except BaseException as e:
             return False
 
-    def insert_one(self, document_name, data, default_permission, user_id):
+    def insert_one(self, document_name, data, user_id=None, default_permission=None):
         try:
             Model = self.get_document(document_name)
             mod = Model(**data)
-            mod.CreatedBy = user_id
-            mod.LastUpdateBy = user_id
 
-            for item in SECURITY_ROLES_ATTRIBUTES:
-                roles = default_permission.get(item, [])
-                setattr(mod, item, roles)
+            if self.is_base_instance(document_name):
 
-            for item in SECURITY_IDS_ATTRIBUTES:
-                ids = default_permission.get(item, [])
-                ids = self.__modify_ids(ids, user_id)
-                setattr(mod, item, ids)
+                if user_id:
+                    mod.CreatedBy = user_id
+                    mod.LastUpdateBy = user_id
+                    for item in settings.SECURITY_IDS_ATTRIBUTES:
+                        ids = default_permission.get(item, [])
+                        ids = self.__modify_ids(ids, user_id)
+                        setattr(mod, item, ids)
 
-            # Only Required on Entity Default Permission Settings for
-            # Writing / Inserting Documents
-            # Otherwise it's ok to insert EMPTY LIST
-            setattr(mod, 'RolesAllowedToWrite', [])
-            setattr(mod, 'IdsAllowedToWrite', [])
+                if default_permission:
+                    for item in settings.SECURITY_ROLES_ATTRIBUTES:
+                        roles = default_permission.get(item, [])
+                        setattr(mod, item, roles)
 
-            mod.save()
-            return False, mod.ItemId
+                    # Only Required on Entity Default Permission Settings for
+                    # Writing / Inserting Documents
+                    # Otherwise it's ok to insert EMPTY LIST
+
+                    setattr(mod, 'RolesAllowedToWrite', [])
+                    setattr(mod, 'IdsAllowedToWrite', [])
+
+                mod.save()
+                return False, mod.ItemId
+            else:
+                mod.save()
+                return False, 'Inserted'
+
         except BaseException as e:
             return True, e
 
-    def insert_many(self, document_name, data_array, default_permission, user_id):
+    def insert_many(self, document_name, data_array, user_id=None, default_permission=None):
         results = []
         for data in data_array:
-            err, item_id = self.insert_one(document_name, data, default_permission, user_id)
+            err, item_id = self.insert_one(document_name, data, user_id, default_permission)
             if err:
                 results.append(None)
             else:
