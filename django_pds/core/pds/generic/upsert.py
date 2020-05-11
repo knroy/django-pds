166c762cd3b6c8d33da80ce9d7df5457c47a7dfb
Komol Nath Roy <rax.komol@gmail.com>
2020-05-11 21:01:43 +0600

    upsert updated

--- django_pds/core/pds/generic/upsert.py
+++ django_pds/core/pds/generic/upsert.py
@@ -1,22 +1,22 @@
 from django_pds.core.controllers import GenericInsertCommandController
-from django_pds.core.rest.response import error_response
-from .update import data_update_api_view_helper
-from .write import data_insert_api_view_helper
+from .update import data_update
+from .write import data_insert
 
 
-def data_upsert_api_view_helper(user_id, document_name, json_string):
+def data_upsert(document_name, data_json, user_id=None, ignore_security=False, force_insert=False):
     try:
         insert_ctrl = GenericInsertCommandController()
-        err, data = insert_ctrl.json_load(json_string)
+        err, data_or_error = insert_ctrl.json_load(data_json)
         if err:
-            return True, error_response(err)
+            return True, data_or_error
 
+        data = data_or_error
         already_exists = insert_ctrl.already_exists(document_name, data.get('ItemId', None))
 
         if not already_exists:
-            return data_insert_api_view_helper(user_id, document_name, json_string)
+            return data_insert(user_id, document_name, data_json, ignore_security, force_insert)
         else:
-            return data_update_api_view_helper(user_id, document_name, json_string)
+            return data_update(user_id, document_name, data_json, ignore_security)
 
     except BaseException as e:
-        return True, error_response(e)
+        return True, str(e)
