a12a7a96ff86fbf4d67bcc9051773ece5f13fe4a
Komol Nath Roy <rax.komol@gmail.com>
2020-05-15 21:34:39 +0600

    UserId field added in User Role Map

--- django_pds/models/userrolemaps.py
+++ django_pds/models/userrolemaps.py
@@ -7,5 +7,6 @@ class UserRoleMap(BaseDocument):
 
     RoleName = StringField(required=True)
     RoleId = StringField(required=False, max_length=36)
+    UserId = StringField(required=True, max_length=36)
 
     meta = {'collection': 'UserRoleMaps'}
