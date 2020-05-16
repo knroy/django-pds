d270fe32b3483d8228964178d9f12ebcbd5156ab
Komol Nath Roy <rax.komol@gmail.com>
2020-05-16 21:30:07 +0600

    version changed to 0.0.4

--- django_pds/__init__.py
+++ django_pds/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Django Platform Data Service'
-__version__ = '0.0.1'
+__version__ = '0.0.4'
 __author__ = 'Komol Nath Roy'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 Komol Nath Roy'
