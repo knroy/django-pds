1202a16fb865d6afb6a8421810a2256a51351be6
Komol Nath Roy <rax.komol@gmail.com>
2020-08-04 10:37:54 +0600

    version upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL = "https://github.com/knroy/django-pds"
 EMAIL = "rax.komol@gmail.com   "
 AUTHOR = "Komol Nath Roy"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 # What packages are required for this module to be executed?
 
