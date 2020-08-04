db0d21da934f1ccfbbbeb7a280d3ad5c117b6c3c
Komol Nath Roy <rax.komol@gmail.com>
2020-08-04 10:37:23 +0600

    bug fixed

--- django_pds/models/__init__.py
+++ django_pds/models/__init__.py
@@ -1,13 +1,11 @@
 from .entity import Entity
 from .entitydefaultpermissionsettings import EntityDefaultPermissionSetting
-from .role import Role
 from .userreadabledata import UserReadableData
 from .userrolemaps import UserRoleMap
 
 __all__ = (
     'Entity',
     'EntityDefaultPermissionSetting',
-    'Role',
     'UserReadableData',
     'UserRoleMap'
 )
